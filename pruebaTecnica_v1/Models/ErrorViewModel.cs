8e03943eb5c49f36b714633b88330777b521a13f
Monique <80782598+monicaardila@users.noreply.github.com>
2022-11-10 16:49:38 -0500

    Delete pruebaTecnica_v1 directory

--- pruebaTecnica_v1/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace pruebaTecnica_v1.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
