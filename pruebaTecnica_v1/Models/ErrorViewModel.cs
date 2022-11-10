24b93f0d2e39e4deba571be5cff7a54f614cb29b
monicaardila <monicayisethardilagudelo@gmail.com>
2022-11-10 16:44:33 -0500

    se termina crud .net

---
+++ pruebaTecnica_v1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace pruebaTecnica_v1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
